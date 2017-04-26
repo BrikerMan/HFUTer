4fdf230350ecee7c604057cfd6d21981756d17ef
Eliyar Eziz <eliyar917@gmail.com>
2017-04-26 16:52:19 +0800

    update

---
+++ HFUTer3/Main/Home/Schedule/View/FLScheduleNewView.swift
@@ -0,0 +1,17 @@
+//
+//  FLScheduleNewView.swift
+//  HFUTer3
+//
+//  Created by BrikerMan on 2017/4/26.
+//  Copyright © 2017年 Eliyar Eziz. All rights reserved.
+//
+
+import UIKit
+
+class FLScheduleNewView: UIView {
+
+    
+    
+    
+
+}
